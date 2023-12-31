bf5af786f63062928a9db9eef020994c13371936
Said Trujillo <said.trujillo@estudiants.urv.cat>
2023-12-31 01:01:19 +0100

    Refactoring and comments addition for NaturalSelection.java.

--- src/Model/operators/selection/NaturalSelection.java
+++ src/Model/operators/selection/NaturalSelection.java
@@ -1,6 +1,7 @@
 package Model.operators.selection;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -9,32 +10,56 @@ import Model.Individuals.CityTilesetPopulation;
 
 public class NaturalSelection {
 
-	public CityTilesetPopulation apply(
-            CityTilesetPopulation pop, 
-            Random generator){
-        
-        //Get the population sorted by fitness
-        List<CityTileset> sortedPop = pop.sortPopulationByFitness();
-        List<Integer> matingPool = new ArrayList<>();
-        double maxFit = sortedPop.get(0).getFitness();
-        int cont = 0;
-        for(CityTileset city : pop) {
-        	int n = Double.valueOf(city.getFitness() * 100 / maxFit).intValue();
-        	for (int i = 0; i < n; i++) {
-        		matingPool.add(cont);
-        	}
-        	cont++;
-        }
-                
-        CityTilesetPopulation aux = new CityTilesetPopulation(pop.getId()+1,pop.size());
-        
-        int selection, random;
-        while(aux.size() < aux.getMaxSize()){
-        	random = generator.nextInt(matingPool.size());
-        	selection = matingPool.get(random);
-            aux.add(sortedPop.get(selection));
-        }
-            
-        return aux;
-    }
+	public CityTilesetPopulation apply(CityTilesetPopulation pop, Random generator) {
+	    // Get the population sorted by fitness
+	    List<CityTileset> sortedPop = pop.sortPopulationByFitness();
+	    
+	    // Create mating pool based on fitness
+	    List<Integer> matingPool = createMatingPool(pop, sortedPop);
+
+	    // Initialize offspring population
+	    CityTilesetPopulation aux = new CityTilesetPopulation(pop.getId() + 1, pop.size());
+
+	    // Select individuals from the mating pool to form the next generation
+	    selectFromMatingPool(aux, matingPool, sortedPop, generator);
+
+	    return aux;
+	}
+
+	/**
+	 * Create a mating pool based on the fitness of individuals in the population.
+	 *
+	 * @param pop       Original population.
+	 * @param sortedPop Population sorted by fitness.
+	 * @return Mating pool indices.
+	 */
+	private List<Integer> createMatingPool(CityTilesetPopulation pop, List<CityTileset> sortedPop) {
+	    List<Integer> matingPool = new ArrayList<>();
+	    double maxFit = sortedPop.get(0).getFitness();
+
+	    for (int cont = 0; cont < pop.size(); cont++) {
+	        int n = Double.valueOf(pop.getArrayList().get(cont).getFitness() * 100 / maxFit).intValue();
+	        matingPool.addAll(Collections.nCopies(n, cont));
+	    }
+
+	    return matingPool;
+	}
+
+	/**
+	 * Select individuals from the mating pool to form the next generation.
+	 *
+	 * @param aux       Offspring population.
+	 * @param matingPool Mating pool indices.
+	 * @param sortedPop Population sorted by fitness.
+	 * @param generator Random number generator.
+	 */
+	private void selectFromMatingPool(CityTilesetPopulation aux, List<Integer> matingPool,
+	                                   List<CityTileset> sortedPop, Random generator) {
+	    while (aux.size() < aux.getMaxSize()) {
+	        int random = generator.nextInt(matingPool.size());
+	        int selection = matingPool.get(random);
+	        aux.add(sortedPop.get(selection));
+	    }
+	}
+
 }
